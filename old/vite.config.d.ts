e6b1df94a92b6734e286471110d65d1fb27b60cb
xiaoxiao-cvs <2573270370@qq.com>
2025-10-26 19:13:57 +0800

    refactor(old): 将old文件夹转移到本地，添加进过滤列表
    feat(app.vue)：创建初始项目

--- old/vite.config.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserConfigExport } from "vite"; // 从 'vite' 导入 UserConfigExport 而不是 UserConfig
-
-// 将类型声明更改为 UserConfigExport，以允许配置是一个函数
-declare const config: UserConfigExport;
-export default config;
